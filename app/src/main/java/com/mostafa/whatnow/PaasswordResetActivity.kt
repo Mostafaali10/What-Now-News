9c82ab143293e87364f2b21fb3df3c77196ab9ee
Mostafa Ali <mostafaali9976@gmail.com>
2025-03-19 16:09:13 +0200

    Adding Password Reset Screen

--- app/src/main/java/com/mostafa/whatnow/PaasswordResetActivity.kt
+++ app/src/main/java/com/mostafa/whatnow/PaasswordResetActivity.kt
@@ -1,20 +1,53 @@
 package com.mostafa.whatnow
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.mostafa.whatnow.databinding.ActivityPaasswordResetBinding
 
 class PaasswordResetActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPaasswordResetBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_paassword_reset)
+        binding = ActivityPaasswordResetBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+       // Firebase Auth
+        auth = Firebase.auth
+
+        binding.resetBtn.setOnClickListener {
+            binding.progress.isVisible= true
+            val email = binding.forgetEmailEt.text.toString()
+            Firebase.auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Check Your Email ", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this,LoginActivity::class.java))
+                        binding.progress.isVisible = false
+                    }
+                }
+        }
+
+
+
+
+
     }
 }
